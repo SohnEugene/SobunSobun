d445183b8e4ee55a8ca9167f1ce5e92636ed267b
SohnEugene <qeugene2328@gmail.com>
2025-12-03 17:19:11 +0900

    fix: edit container page for testing environment

--- frontend-kiosk/src/pages/ContainerPurchasePage.jsx
+++ frontend-kiosk/src/pages/ContainerPurchasePage.jsx
@@ -23,20 +23,16 @@ export default function ContainerPurchasePage({ onNext, onHome }) {
       <KioskHeader onHome={onHome} />
       <div className="kiosk-content">
         <div className="kiosk-title">
-          리필 전용 다회용기를
+          아래에 비치된 용기를
           <br />
-          추가하시겠어요?
+          이용해주세요
         </div>
         <img className="bottle-image" src={bottlesImage} alt="" />
       </div>
       <div className="kiosk-footer">
         <div className="kiosk-button-container">
           <Button variant="primary" onClick={handlePurchase}>
-            추가하기 (500원)
-          </Button>
-
-          <Button onClick={handleSkip} variant="outlined">
-            건너뛰기
+            리필 시작하기
           </Button>
         </div>
       </div>
